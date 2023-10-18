55d3fd51263eeef091cbe5ee14f1c7e93224bf6b
Udith Chandrarathna <udithsc@gmail.com>
2023-10-18 12:56:07 +0800

    added genre list filtering

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,50 @@
+import {
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Spinner,
+  Button,
+} from '@chakra-ui/react';
+import useGenres, { Genre } from '../hooks/useGenres';
+import getCroppedImageUrl from '../services/image-url';
+
+interface Props {
+  onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
+}
+
+const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
+  const { data: genres, isLoading, error } = useGenres();
+
+  if (error) return null;
+  if (isLoading) return <Spinner />;
+
+  return (
+    <List>
+      {genres.map((genre) => (
+        <ListItem key={genre.id} paddingY={'5px'}>
+          <HStack>
+            <Image
+              boxSize={'32px'}
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <Button
+              whiteSpace='normal'
+              textAlign='left'
+              fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'}
+              fontSize='lg'
+              variant='link'
+              onClick={() => onSelectGenre(genre)}
+            >
+              {genre.name}
+            </Button>
+          </HStack>
+        </ListItem>
+      ))}
+    </List>
+  );
+};
+
+export default GenreList;
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,5 @@
-import { useEffect, useState } from 'react';
-import apiClient from '../services/api-client';
-import { CanceledError } from 'axios';
+import useData from './useData';
+import { Genre } from './useGenres';
 
 export interface Platform {
   id: number;
@@ -18,35 +17,15 @@ export interface Game {
   metacritic: number;
 }
 
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
-
-const useGames = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState('');
-  const [isLoading, setisLoading] = useState(false);
-
-  useEffect(() => {
-    const controller = new AbortController();
-    setisLoading(true);
-    apiClient
-      .get<FetchGamesResponse>('/games', { signal: controller.signal })
-      .then((res) => {
-        setGames(res.data.results);
-        setisLoading(false);
-      })
-      .catch((error) => {
-        if (error instanceof CanceledError) return;
-        setError(error.message);
-        setisLoading(false);
-      });
-
-    return () => controller.abort();
-  }, []);
-
-  return { games, error, isLoading };
-};
+const useGames = (selectedGenre: Genre | null) =>
+  useData<Game>(
+    '/games',
+    {
+      params: {
+        genres: selectedGenre?.id,
+      },
+    },
+    [selectedGenre?.id]
+  );
 
 export default useGames;
