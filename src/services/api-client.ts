cc2851feed4b19fc312785b79ea2f23bcbd09e91
Udith Chandrarathna <udithsc@gmail.com>
2025-06-24 21:09:24 +0800

    code formatted

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
-    key: '49210ae6f6a64c5d8ed480273d79b26b',
+    key: import.meta.env.VITE_RAWG_API_KEY,
   },
 });
